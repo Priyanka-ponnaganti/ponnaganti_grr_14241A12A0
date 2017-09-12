18d8f059145f31cecac91f8855758e973fc45079
Priyanka-ponnaganti <31859093+Priyanka-ponnaganti@users.noreply.github.com>
2017-09-12 13:13:03 +0530

    Add files via upload

---
+++ StudentGroup.java
@@ -0,0 +1,171 @@
+import java.util.Date;
+
+
+public class StudentGroup implements StudentArrayOperation {
+
+	private Student[] students;
+	
+	
+	public StudentGroup(int length) {
+		this.students = new Student[length];
+	}
+
+	@Override
+	public Student[] getStudents() {
+
+
+
+
+         
+		
+	return null;
+}
+	
+
+	@Override
+	public void setStudents(Student[] students) {
+		
+
+	}
+
+	@Override
+	public Student getStudent(int index) {
+		// Add your implementation here
+		return null;
+	}
+
+	@Override
+	public void setStudent(Student student, int index) {
+		else if(Student[]==index)
+                 System.out.println("throws"+index);
+
+	}
+
+	@Override
+	public void addFirst(Student student) {
+Scanner s=new Scanner("System.in");
+Student student=new Student();
+String name;
+String address;
+name=s.nextLine();
+address=s.nextLine();
+		
+	}
+
+	@Override
+	public void addLast(Student student) {
+for(int i=0;i<=student;i++)
+{
+scanner s2=new Scanner("System.in");
+System.out.println("enter you want to add at last");
+String name;
+String address;
+name =s.nextInt();		
+	}
+
+	@Override
+	public void add(Student student, int index) {
+		this.Student=arr[Student,Student+index];// Add your implementation here
+	}
+
+	@Override
+	public void remove(int index) {
+Scanner s3=new Scanner("System.in");
+for(i=0;i<=student;i++)
+{
+if(student==index)
+{
+student--;}
+
+
+// Add your implementation here
+	}
+
+	/*@Override
+	public void remove(Student student) {
+Scanner s4=new Scanner("System.in");
+for(i=0;i<=student;i++)
+student--;
+		// Add your implementation here
+	}
+
+	@Override
+	public void removeFromIndex(int index) {
+Scanner s5=new Scanner("System.in");
+String name;
+name s5=nextLine();
+if(s5==index[i])
+{ System.out.println("remove from index");
+index[i]==index[i]-1;
+		// Add your implementation here
+	}
+
+	@Override
+	public void removeFromElement(Student student) {
+Scanner s6=new Scanner("System.in");
+String name;
+for(i=0;i<student;i++)
+{
+if(s6==index[i])
+System.out.println("index removed from elements");
+
+		// Add your implementation here
+	}
+
+	@Override
+	public void removeToIndex(int index) {
+		// Add your implementation here
+	}
+
+	@Override
+	public void removeToElement(Student student) {
+		// Add your implementation here
+	}
+
+	@Override
+	public void bubbleSort() {
+		// Add your implementation here
+	}
+
+	@Override
+	public Student[] getByBirthDate(Date date) {
+		// Add your implementation here
+		return null;
+	}
+
+	@Override
+	public Student[] getBetweenBirthDates(Date firstDate, Date lastDate) {
+		// Add your implementation here
+		return null;
+	}
+
+	@Override
+	public Student[] getNearBirthDate(Date date, int days) {
+		// Add your implementation here
+		return null;
+	}
+
+	@Override
+	public int getCurrentAgeByDate(int indexOfStudent) {
+		// Add your implementation here
+		return 0;
+	}
+
+	@Override
+	public Student[] getStudentsByAge(int age) {
+		// Add your implementation here
+		return null;
+	}
+
+	@Override
+	public Student[] getStudentsWithMaxAvgMark() {
+		// Add your implementation here
+		return null;
+	}
+
+	@Override
+	public Student getNextStudent(Student student) {
+		// Add your implementation here
+		return null;
+	}*/
+}
